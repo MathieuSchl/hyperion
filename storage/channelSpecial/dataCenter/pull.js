f0382edd03a9d65651c7d735036f399773bc4ec6
Renorecraft <mathieuman78@gmail.com>
2020-12-27 11:02:32 +0100

    pull.js can now send messages with more than 2000 characters

--- storage/channelSpecial/dataCenter/pull.js
+++ storage/channelSpecial/dataCenter/pull.js
@@ -1,14 +1,28 @@
-const { exec } = require('child_process');
+const {
+    exec
+} = require('child_process');
 const config = require('../../config.json');
 
 
 module.exports.run = async (bot, message, args) => {
     message.delete();
-     exec('git pull', {cwd: config.location}, function (error, stdout, stderr) {
-        console.log(error);
-        console.log(stdout);
-        console.log(stderr);
-        message.channel.send("```"+stdout+"```").catch(()=>{message.channel.send("```Une grosse mise à jour a été faite```")});
+    exec('git pull', {
+        cwd: config.location
+    }, function (error, stdout, stderr) {
+        //console.log(error);
+        //console.log(stdout);
+        //console.log(stderr);
+        const stdoutSplit = stdout.split("\n")
+        let msg = "";
+        for (let index = 0; index < stdoutSplit.length; index++) {
+            const element = stdoutSplit[index];
+            if (msg.length + element.length <= 1900) {
+                msg = msg + element + "\n";
+            } else {
+                message.channel.send("```" + msg + "```");
+                msg = "";
+            }
+        }
     });
 };
 
