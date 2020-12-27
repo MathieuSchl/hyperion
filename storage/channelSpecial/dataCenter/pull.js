c03bffea072d41bf2d367550fd39f7471dbba977
Renorecraft <mathieuman78@gmail.com>
2020-12-27 09:02:03 +0100

    Fix in pull.js and reboot.js

--- storage/channelSpecial/dataCenter/pull.js
+++ storage/channelSpecial/dataCenter/pull.js
@@ -5,9 +5,9 @@ const config = require('../../config.json');
 module.exports.run = async (bot, message, args) => {
     message.delete();
      exec('git pull', {cwd: config.location}, function (error, stdout, stderr) {
-        //console.log(error);
-        //console.log(stdout);
-        //console.log(stderr);
+        console.log(error);
+        console.log(stdout);
+        console.log(stderr);
         message.channel.send("```"+stdout+"```").catch(()=>{message.channel.send("```Une grosse mise à jour a été faite```")});
     });
 };
--- storage/channelSpecial/dataCenter/reboot.js
+++ storage/channelSpecial/dataCenter/reboot.js
@@ -4,7 +4,7 @@
 module.exports.run = async (bot, message, args)=>{
     message.delete();
     message.channel.send("```Redémarage dans 5s```").then((msg)=>{msg.delete({ timeout:5000 })});
-    await bot.basicFunctions.get("wait").run(1000);
+    await bot.basicFunctions.get("wait").run(5200);
     bot.destroy();
     require('child_process').exec('sudo /sbin/shutdown -r now', function (msg) { console.log(msg) });
 };
