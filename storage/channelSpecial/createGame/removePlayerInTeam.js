79a767bfaa4a973c4c64d78973a7bf6c3807ee36
Renorecraft <mathieuman78@gmail.com>
2020-12-27 22:09:14 +0100

    Fixing bugs whan players leave a game

--- storage/channelSpecial/createGame/removePlayerInTeam.js
+++ storage/channelSpecial/createGame/removePlayerInTeam.js
@@ -1,48 +1,49 @@
-
-
 module.exports.run = async (bot, playerId, gameId, byHost) => {
     let teamData = await bot.basicFunctions.get("teamData").open(gameId);
 
-    
-    const indexPlayer = teamData.players.indexOf(playerId)
-    teamData.players.splice(indexPlayer,1);
 
-    await bot.basicFunctions.get("teamData").write(gameId,teamData);
+    const indexPlayer = teamData.players.indexOf(playerId)
+    teamData.players.splice(indexPlayer, 1);
+
+    await bot.basicFunctions.get("teamData").write(gameId, teamData);
 
     const gameChannel = await bot.channels.fetch(teamData.id);
     const player = await gameChannel.guild.members.fetch(playerId);
     player.roles.remove(teamData.role)
 
-    
-    if(byHost===true) player.user.send(`Vous avez été retiré de la partie \`${teamData.name}\``);
+
+    if (byHost === true) player.user.send(`Vous avez été retiré de la partie \`${teamData.name}\``);
     else player.user.send(`Vous avez quitté la partie \`${teamData.name}\``);
 
-    if(teamData.players.length===0) bot.specialChannel.createGame.get("removeGame").run(bot, gameId);
+    if (teamData.players.length === 0) bot.specialChannel.createGame.get("removeGame").run(bot, gameId);
 
-    if(indexPlayer!==0) return;
-    if(teamData.players.length===0) return;
+    if (indexPlayer !== 0) return;
+    if (teamData.players.length === 0) return;
 
-    const newHostUser = await gameChannel.guild.members.fetch(teamData.players[0])
+    try {
+        const newHostUser = await gameChannel.guild.members.fetch(teamData.players[0])
 
-    const invitePlayerChannel = await bot.channels.fetch(teamData.invitePlayer)
-    const removePlayerChannel = await bot.channels.fetch(teamData.removePlayer)
-    
-    const permissionsInvite = invitePlayerChannel.permissionOverwrites.get(playerId);
-    permissionsInvite.id = teamData.players[0];
-    permissionsInvite.update(permissionsInvite);
+        const invitePlayerChannel = await bot.channels.fetch(teamData.invitePlayer)
+        const removePlayerChannel = await bot.channels.fetch(teamData.removePlayer)
 
-    const allPermissionsInvite = invitePlayerChannel.permissionOverwrites.filter(o => o.id !== playerId).array();
-    invitePlayerChannel.overwritePermissions(allPermissionsInvite);
+        const permissionsInvite = invitePlayerChannel.permissionOverwrites.get(playerId);
+        console.log(teamData.players[0])
+        permissionsInvite.id = teamData.players[0];
+        permissionsInvite.update(permissionsInvite);
 
-    
-    const permissionsDelete = removePlayerChannel.permissionOverwrites.get(playerId);
-    permissionsDelete.id = teamData.players[0];
-    permissionsDelete.update(permissionsDelete);
+        const allPermissionsInvite = invitePlayerChannel.permissionOverwrites.filter(o => o.id !== playerId).array();
+        invitePlayerChannel.overwritePermissions(allPermissionsInvite);
 
-    const allPermissionsDelete = removePlayerChannel.permissionOverwrites.filter(o => o.id !== playerId).array();
-    removePlayerChannel.overwritePermissions(allPermissionsDelete);
 
-    newHostUser.send(`Vous êtes le nouveau chef de la partie \`${teamData.name}\`\nVous pouvez inviter d'autres joueur grâce au salon \`inviter-des-joueurs\``);
+        const permissionsDelete = removePlayerChannel.permissionOverwrites.get(playerId);
+        permissionsDelete.id = teamData.players[0];
+        permissionsDelete.update(permissionsDelete);
+
+        const allPermissionsDelete = removePlayerChannel.permissionOverwrites.filter(o => o.id !== playerId).array();
+        removePlayerChannel.overwritePermissions(allPermissionsDelete);
+
+        newHostUser.send(`Vous êtes le nouveau chef de la partie \`${teamData.name}\`\nVous pouvez inviter d'autres joueur grâce au salon \`inviter-des-joueurs\``);
+    } catch {}
 }
 
 module.exports.help = {
--- storage/messageSpecial/leaveGame/index.js
+++ storage/messageSpecial/leaveGame/index.js
@@ -1,10 +1,13 @@
 
 
+
 module.exports.run = async (bot, reaction, user, action, dataSpecialMessage) => {
-    bot.specialChannel.createGame.get("removePlayerInTeam").run(bot, user.id, dataSpecialMessage.data.gameId);
-    await bot.basicFunctions.get("wait").run(5000);
-    let teamData = await bot.basicFunctions.get("teamData").open(reaction.message.channel.id);
-    if(teamData.type==="startGame") bot.specialChannel.startMenu.get("menu").run(bot, teamData.id);
+    try {
+        bot.specialChannel.createGame.get("removePlayerInTeam").run(bot, user.id, dataSpecialMessage.data.gameId);
+        await bot.basicFunctions.get("wait").run(5000);
+        let teamData = await bot.basicFunctions.get("teamData").open(reaction.message.channel.id);
+        if (teamData.type === "startGame") bot.specialChannel.startMenu.get("menu").run(bot, teamData.id);
+    } catch {}
 }
 
 module.exports.help = {
