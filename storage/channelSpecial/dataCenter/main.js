8a0a26691729574e7068c31271b633c10a8ea357
Renorecraft <mathieuman78@gmail.com>
2021-01-01 16:01:09 +0100

    The bot is now reboot after changing mode and pull

--- storage/channelSpecial/dataCenter/main.js
+++ storage/channelSpecial/dataCenter/main.js
@@ -6,24 +6,14 @@ const config = require('../../config.json');
 
 module.exports.run = async (bot, message, args) => {
     message.delete();
-    exec('git pull', {
+    exec('git checkout main', {
         cwd: config.location
     }, function (error, stdout, stderr) {
         console.log(error);
         //console.log(stdout);
         console.log(stderr);
-        const stdoutSplit = stdout.split("\n")
-        let msg = "";
-        for (let index = 0; index < stdoutSplit.length; index++) {
-            const element = stdoutSplit[index];
-            if (msg.length + element.length <= 1900) {
-                msg = msg + element + "\n";
-            } else {
-                message.channel.send("```" + msg + "```");
-                msg = element;
-            }
-        }
-        message.channel.send("```" + msg + "```");
+
+        message.channel.send("```Le bot est maintenant en mode normal```");
         await bot.basicFunctions.get("wait").run(250);
         bot.destroy();
         await bot.basicFunctions.get("wait").run(5000);
@@ -32,5 +22,5 @@ module.exports.run = async (bot, message, args) => {
 };
 
 module.exports.help = {
-    name: "pull"
+    name: "main"
 };
