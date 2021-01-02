b4cd9c2fd6dae6eb8ebc8cdf0d0f0b38225a88d4
Renorecraft <mathieuman78@gmail.com>
2021-01-02 14:07:37 +0100

    Message from pull goes to a logs channel

--- storage/channelSpecial/dataCenter/pull.js
+++ storage/channelSpecial/dataCenter/pull.js
@@ -2,29 +2,57 @@ const {
     exec
 } = require('child_process');
 const config = require('../../config.json');
+const idGitChannel = "794860616665137162";
 
 
-module.exports.run = async (bot, message, args) => {
-    message.delete();
+async function pull(bot, GitChannel, terminalChannel) {
     exec('git pull', {
         cwd: config.location
-    }, function (error, stdout, stderr) {
-        console.log(error);
+    }, async function (error, stdout, stderr) {
+        //console.log(error);
         //console.log(stdout);
-        console.log(stderr);
-        const stdoutSplit = stdout.split("\n")
+        //console.log(stderr);
+        const stdoutSplit = stdout.split("\n");
+        if (stdoutSplit.length <= 3) {
+            if(terminalChannel!=null) terminalChannel.send("Déjà à jour").then(async(msg) => {
+                await bot.basicFunctions.get("wait").run(30000);
+                if(!msg.deleted) msg.delete();
+            });
+            return;
+        }
         let msg = "";
         for (let index = 0; index < stdoutSplit.length; index++) {
             const element = stdoutSplit[index];
             if (msg.length + element.length <= 1900) {
                 msg = msg + element + "\n";
             } else {
-                message.channel.send("```" + msg + "```");
+                GitChannel.send("```" + msg + "```");
                 msg = element;
             }
         }
-        message.channel.send("```" + msg + "```");
+        GitChannel.send("```" + msg + "```");
+        if(terminalChannel!=null) terminalChannel.send("Mise à jour terminé, reboot dans 5s").then(async(msg) => {
+            await bot.basicFunctions.get("wait").run(5000);
+            if(!msg.deleted) msg.delete();
+        });
+        await bot.basicFunctions.get("wait").run(5250);
+        bot.destroy();
+        await bot.basicFunctions.get("wait").run(5000);
+        require('child_process').exec(`node ${config.location}/index.js`, function (msg) {
+            console.log(msg)
+        });
     });
+}
+
+module.exports.run = async (bot, message, args) => {
+    message.delete();
+    const GitChannel = await bot.channels.fetch(idGitChannel);
+    pull(bot, GitChannel, message.channel);
+};
+
+module.exports.ready = async (bot) => {
+    const GitChannel = await bot.channels.fetch(idGitChannel);
+    pull(bot, GitChannel, null);
 };
 
 module.exports.help = {
