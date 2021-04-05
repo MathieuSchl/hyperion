c0f27c634c8a5a360ff06b769b09485f2140ce70
CodyIsTheSenate <mathieuman78@gmail.com>
2021-04-05 22:42:44 +0200

    Change autorisation to destroy the bot client

--- storage/commands/destroy.js
+++ storage/commands/destroy.js
@@ -2,11 +2,24 @@ const Discord = require("discord.js");
 const config = require("../config.json");
 const admin = ["210392675478667269"]
 
-module.exports.run = async (bot, message, args)=>{
+module.exports.run = async (bot, message, args) => {
     message.delete();
+    if (!admin.includes(message.author.id)) {
+        message.channel.send("Tu n'es pas autorisé à faire ca !").then(async (msg) => {
+            await bot.basicFunctions.get("wait").run(10000);
+            if (!msg.deleted) msg.delete();
+        })
+        return;
+    }
     message.channel.send("Arrêt du bot")
-        .then(msg => {msg.delete({ timeout: 1500 })});
-    bot.user.setActivity("Arrêt en cours", {type: "WATCHING"});
+        .then(msg => {
+            msg.delete({
+                timeout: 1500
+            })
+        });
+    bot.user.setActivity("Arrêt en cours", {
+        type: "WATCHING"
+    });
     await bot.basicFunctions.get("wait").run(2500);
     bot.destroy();
 };
@@ -19,7 +32,7 @@ module.exports.help = {
 module.exports.manuel = {
     man: "man permet d'afficher le manuel d'une fonction\n" +
         "Pour ce faire il faut écrire dans le terminal:\n" +
-        "`"+config.prefix+" man [nom de la commande]`"
+        "`" + config.prefix + " man [nom de la commande]`"
 };
 
 module.exports.manuelview = {
