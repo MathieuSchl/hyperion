22e327afec20112eee5df35d2da96cef2d95f089
CodyIsTheSenate <mathieuman78@gmail.com>
2021-05-01 09:26:02 +0200

    Add a new function to reload connection

--- enventIndex/createDBConnection.js
+++ enventIndex/createDBConnection.js
@@ -5,6 +5,16 @@ module.exports.run = async (bot, callback) => {
     });
 };
 
+module.exports.reaload = async (bot, callback) => {
+    const dbToClose = bot.db;
+    require("../../../dataBase/connection.js").getDb(async (db) => {
+        bot.db = db;
+        callback();
+        await bot.basicFunctions.get("wait").run(3000);
+        dbToClose.end();
+    });
+};
+
 module.exports.help = {
     name: "createDBConnection"
 };
