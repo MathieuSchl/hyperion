ecafd41138a63162f4566d28ca4b1d979bea1f82
CodyIsTheSenate <mathieuman78@gmail.com>
2021-11-16 15:55:17 +0100

    Fix catch message and channel

--- enventIndex/catchMessageInSpecialChannels.js
+++ enventIndex/catchMessageInSpecialChannels.js
@@ -9,60 +9,63 @@ module.exports.run = async (bot) => {
 
     for (let index = 0; index < allGuilds.length; index++) {
         const element = allGuilds[index];
-        if(element.rulesChannelID!=null){
+        if (element.rulesChannelID != null) {
             channel = await bot.channels.fetch(element.rulesChannelID);
             channel.messages.fetch();
         }
     }
 
-    await fs.readdir(pathSpecialChannels, async function (err, files) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        for (let i = 0; i < files.length; i++) {
-            files[i] = files[i].split(".")[0];
-        }
-        for (let i = 0; i < files.length; i++) {
-            let channel = await bot.channels.fetch(files[i]);
-            if(channel==null){
-                console.log("Channel "+files[i]+" does not exist");
-            }else{
-                channel.messages.fetch();
+    await new Promise((resolve, reject) => {
+        fs.readdir(pathSpecialChannels, async function (err, files) {
+            if (err) {
+                console.log(err);
+                return;
             }
-        }
-    });
+            for (let i = 0; i < files.length; i++) {
+                files[i] = files[i].split(".")[0];
+            }
+            for (let i = 0; i < files.length; i++) {
+                await bot.channels.fetch(files[i]).then((channel) => {
+                    channel.messages.fetch();
+                }).catch(() => {
+                    console.log("Channel " + files[i] + " does not exist");
+                })
+            }
+            resolve();
+        });
+    })
 
     fichiers = fs.readFileSync(pathMpChannel);
     let donnePath = JSON.parse(fichiers);
 
     let keys = Object.keys(donnePath);
     for (let theChannel of keys) {
-        channel = await bot.channels.fetch(donnePath[theChannel]);
-        if(channel==null){
-            console.log("Channel "+theChannel+" does not exist");
-        }else{
-            channel.messages.fetch();
-        }
+        channel = await bot.channels.fetch(donnePath[theChannel]).then(async function (channel) {
+            await channel.messages.fetch();
+        }).catch(function (err) {
+            console.log("Channel " + theChannel + " does not exist");
+        })
     }
 
-    await fs.readdir(pathSpecialMessages, async function (err, files) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        for (let i = 0; i < files.length; i++) {
-            fichiers = fs.readFileSync(config.location + "/storage/data/specialMessageList/" + files[i]);
-            let dataSpecialMessage = JSON.parse(fichiers);
-
-            let channel = await bot.channels.fetch(dataSpecialMessage.channel);
-            if(channel==null){
-                console.log("Message "+files[i].split(".")[0]+" does not exist");
-            }else{
-                channel.messages.fetch();
+    await new Promise((resolve, reject) => {
+        fs.readdir(pathSpecialMessages, async function (err, files) {
+            if (err) {
+                console.log(err);
+                return;
             }
-        }
-    });
+            for (let i = 0; i < files.length; i++) {
+                fichiers = fs.readFileSync(config.location + "/storage/data/specialMessageList/" + files[i]);
+                let dataSpecialMessage = JSON.parse(fichiers);
+
+                bot.channels.fetch(dataSpecialMessage.channel).then(async function (channel) {
+                    await channel.messages.fetch();
+                }).catch(function (err) {
+                    console.log("Channel " + theChannel + " does not exist");
+                })
+            }
+            resolve();
+        });
+    })
 };
 
 
