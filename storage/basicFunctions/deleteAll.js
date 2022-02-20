dc6df232ba763ade907c6c2a65af7761b6cbc484
CodyIsTheSenate <mathieuman78@gmail.com>
2022-02-20 01:03:48 +0100

    Fix error with delete messages

--- storage/basicFunctions/deleteAll.js
+++ storage/basicFunctions/deleteAll.js
@@ -1,16 +1,21 @@
 module.exports.run = async (bot, channel) => {
     channel.bulkDelete(100).catch(() => {});
-    channel.messages.fetch().then(messages => {
-        messages.array().forEach(msg => {
-            try {
-                setTimeout(function () {
-                    if (!msg.deleted) msg.delete().catch(() => {});
-                }, 100);
-            } catch (error) {
-                console.log("ERROR in delete message catch");
-                console.log(error);
-            }
-        });
+
+    await new Promise((resolve, reject) => {
+        channel.messages.fetch().then(async (messages) => {
+            await bot.basicFunctions.get("wait").run(50);
+            messages.array().forEach(msg => {
+                try {
+                    setTimeout(function () {
+                        if (!msg.deleted) msg.delete().catch(() => {});
+                    }, 100);
+                } catch (error) {
+                    console.log("ERROR in delete message catch");
+                    console.log(error);
+                }
+            });
+            resolve();
+        })
     })
 };
 
--- storage/channelSpecial/archiveAccess_password/checkPassword.js
+++ storage/channelSpecial/archiveAccess_password/checkPassword.js
@@ -1,8 +1,6 @@
-const config = require('../../config.json');
-const fs = require("fs");
 const passwordSystem = "archiveAccess";
-const system = passwordSystem+"_password";
-const passWords = ["obi wan","obiwankenobi","obiwan kenobi","obi wan kenobi","obi wankenobi","obi-wankenobi","obi wan","obi-wan","kenobi","gonk"];
+const system = passwordSystem + "_password";
+const passWords = ["obi wan", "obiwankenobi", "obiwan kenobi", "obi wan kenobi", "obi wankenobi", "obi-wankenobi", "obi wan", "obi-wan", "kenobi", "obi-wan kenobi", "obi wan-kenobi","obi-wan-kenobi", "gonk"];
 
 module.exports.run = async (bot, message, teamData) => {
     let channelMessages = message.channel.messages;
@@ -14,7 +12,7 @@ module.exports.run = async (bot, message, teamData) => {
         }
         messages.array().reverse().forEach(msg => {
             if (i >= 2) {
-                if(!msg.deleted) msg.delete().catch();
+                if (!msg.deleted) msg.delete().catch();
             }
             i++;
         });
@@ -31,7 +29,7 @@ module.exports.run = async (bot, message, teamData) => {
     if (passWords.includes(message.content.toLowerCase())) {
         teamData.data[passwordSystem].unlock = true;
         await bot.basicFunctions.get("teamData").write(message.channel.id, teamData);
-        
+
         message.channel.send("Accepted password\nWelcome on board captain").then(msg => {
             setTimeout(function () {
                 if (!msg.deleted) msg.delete().catch("Erreur delete message is string");
@@ -40,7 +38,7 @@ module.exports.run = async (bot, message, teamData) => {
         await bot.basicFunctions.get("wait").run(5100);
         bot.specialChannel[system].get("unloked").run(bot, message, teamData);
         bot.basicFunctions.get("scoreBoard").reaload(bot);
-        bot.basicFunctions.get("eventLuncher").run(bot,teamData,message);
+        bot.basicFunctions.get("eventLuncher").run(bot, teamData, message);
         return
     } else {
         teamData.data[passwordSystem].nbTest++;
